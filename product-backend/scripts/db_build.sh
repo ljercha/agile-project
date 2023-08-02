b25e9de41e1d7cb13763c24d31702d98536f4149
japilarski <141143566+japilarski@users.noreply.github.com>
2023-08-02 15:50:08 +0200

    Update db_build.sh

--- product-backend/scripts/db_build.sh
+++ product-backend/scripts/db_build.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 source ~/.zshrc
 DB_PORT=3306
-mysql -h ${DB_HOST} -P ${DB_PORT} -u ${DB_USERNAME} -p${DB_PASSWORD} ${DB_NAME} < create_db.sql
-mysql -h ${DB_HOST} -P ${DB_PORT} -u ${DB_USERNAME} -p${DB_PASSWORD} ${DB_NAME} < example_data.sql
+mysql -h ${DB_HOST} -P ${DB_PORT} -u ${DB_USERNAME} -p${DB_PASSWORD} < create_db.sql
+mysql -h ${DB_HOST} -P ${DB_PORT} -u ${DB_USERNAME} -p${DB_PASSWORD} < example_data.sql
