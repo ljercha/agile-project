815ab203b4ed59de4ee82e6c4a077c935d6e0741
kamil adaszewski <kamil.adaszewski@kainos.com>
2023-08-03 22:26:01 +0200

    fix: app.ts

--- product-web-app/app.ts
+++ product-web-app/app.ts
@@ -5,9 +5,6 @@ import session from 'express-session';
 import path from 'path';
 import nunjucks from 'nunjucks';
 
-// import Product from './model/product.js';
-// import productController from './controller/productController.js';
-
 const dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
 const app: Application = express();
@@ -29,7 +26,6 @@ app.use(session({ secret: 'NOT_HARDCODED_SECRET', cookie: { maxAge: 60000 } }));
 
 declare module 'express-session' {
   interface SessionData {
-    product: Partial<Product>;
     token: string;
   }
 }
@@ -40,11 +36,3 @@ app.use('/public', express.static(path.join(dirname, 'public')));
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
 });
-
-// Routing
-//
-// app.get('/', (eq: Request, res: Response) => {
-//   res.redirect('/products');
-// });
-
-// productController(app);
