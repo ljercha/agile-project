d63a0e3c96a6e095829f776883bf74e9574325f9
kamil adaszewski <kamil.adaszewski@kainos.com>
2023-08-03 22:28:38 +0200

    fix: app.ts v2

--- product-web-app/app.ts
+++ product-web-app/app.ts
@@ -1,4 +1,4 @@
-import express, { Application, Request, Response } from 'express';
+import express, { Application} from 'express';
 import * as url from 'url';
 import 'dotenv/config';
 import session from 'express-session';
