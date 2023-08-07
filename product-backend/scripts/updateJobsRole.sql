93d3f8c37f316859c0be8b16c15b9c776fa10aa4
Wojciech Jakimczyk <multipanj@gmail.com>
2023-08-07 14:47:54 +0200

    add: DB script

---
+++ product-backend/scripts/updateJobsRole.sql
@@ -0,0 +1,19 @@
+DROP DATABASE IF EXISTS TeamA_{{USERNAME}};
+CREATE DATABASE TeamA_{{USERNAME}};
+use TeamA_{{USERNAME}};
+
+-- US002: add feat to JobRoles: jobSpecification
+create table JobRoles (
+	job_role_id INT primary key AUTO_INCREMENT,
+    job_role_tit varchar(100)
+);
+
+-- Create Specification table
+CREATE TABLE Specifications (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    role_id INT,
+    summary VARCHAR(255),
+    description TEXT,
+    sharepoint_link VARCHAR(255),
+    FOREIGN KEY (role_id) REFERENCES JobRoles(job_role_id)
+)
