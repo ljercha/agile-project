5c43a75da70af9d9ecd9cbeac8c365ab4f1bd433
japilarski <141143566+japilarski@users.noreply.github.com>
2023-08-09 10:53:11 +0200

    fix create_db.sql

--- product-backend/scripts/create_db.sql
+++ product-backend/scripts/create_db.sql
@@ -4,8 +4,6 @@ use TeamA_{{USERNAME}};
 
 -- US001: view job roles
 create table JobRoles (
-	job_role_id INT primary key AUTO_INCREMENT,
+    job_role_id INT primary key AUTO_INCREMENT,
     job_role_title varchar(100)
 );
-
-select * from JobRoles;
