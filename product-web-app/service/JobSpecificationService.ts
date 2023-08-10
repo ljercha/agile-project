af715cb248d1864d6cedeaec8ff70a57a6bb4da1
Wojciech Jakimczyk <multipanj@gmail.com>
2023-08-10 12:09:59 +0200

    fix: jobSpecService

--- product-web-app/service/JobSpecificationService.ts
+++ product-web-app/service/JobSpecificationService.ts
@@ -2,12 +2,16 @@ import axios from 'axios';
 import JobSpecification from '../model/JobSpecification.js';
 
 export default class JobSpecificationService {
+  private API_URL: string;
+
+  constructor() {
+    this.API_URL = process.env.API_URL || '';
+  }
   this: any;
-  /* eslint-disable */
+  // eslint-disable-next-line class-methods-use-this
   async getJobSpecification(this: any, id: number): Promise<JobSpecification> {
-    /* eslint-enable */
     try {
-      const response = await axios.get(`/api/job-specification/${id}`);
+      const response = await axios.get(`${this.API_URL}/job-specification/${id}`);
       return response.data;
     } catch (e) {
       throw new Error('cant get job specification');
