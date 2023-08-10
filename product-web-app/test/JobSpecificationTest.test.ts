e702ce65e041958aa12eb3a0a4337e5e49e8d978
Wojciech Jakimczyk <multipanj@gmail.com>
2023-08-10 14:52:31 +0200

    fix test

--- product-web-app/test/JobSpecificationTest.test.ts
+++ product-web-app/test/JobSpecificationTest.test.ts
@@ -5,22 +5,22 @@ import JobSpecificationService from '../service/JobSpecificationService.js';
 
 const { expect } = chai;
 const jobSpecificationService = new JobSpecificationService();
-(jobSpecificationService as any).API_URL = 'http://localhost:8080';
+(jobSpecificationService as any).API_URL = 'http://localhost:8080/api';
 
 describe('JobSpecificationService', () => {
   describe('getJobSpecification', () => {
     it('should return the job spec  from response', async () => {
       const mock = new MockAdapter(axios);
       const roleId = 1;
-      mock.onGet(`/api/job-specification/${roleId}`).reply(200, roleId);
+      mock.onGet(`http://localhost:8080/api/job-specification/${roleId}`).reply(200, roleId);
       const results = await jobSpecificationService.getJobSpecification(1);
       expect(results).to.deep.equal(roleId);
     });
 
-    it('should throw exception when 500 error returned from axios', async () => {
+    it('should throw exception when 404 error returned from axios', async () => {
       const mock = new MockAdapter(axios);
       const roleId = 200;
-      mock.onGet(`/api/job-specification/${roleId}`).reply(500);
+      mock.onGet(`/api/job-specification/${roleId}`).reply(404);
       let error: any;
       try {
         await jobSpecificationService.getJobSpecification(200);
