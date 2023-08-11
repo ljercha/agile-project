093144933c969ece2826a992060776a51a9987ef
Lukasz Jercha <lukaszj@kainos.com>
2023-08-11 13:16:22 +0200

    fix: removed db_name

--- product-backend/scripts/db_build.sh
+++ product-backend/scripts/db_build.sh
@@ -5,8 +5,8 @@ db_name="$DB_NAME"
 DB_PORT=3306
 
 mysql -h ${DB_HOST} -D ${DB_SCHEMA} -P ${DB_PORT} -u ${DB_USERNAME} -p${DB_PASSWORD} < create_db.sql
-mysql -h ${DB_HOST} -D ${DB_SCHEMA} -P ${DB_PORT} -u ${DB_USERNAME} -p${DB_PASSWORD}  < example_data.sql
-mysql -h ${DB_HOST} -D ${DB_SCHEMA} -P ${DB_PORT} -u ${DB_USERNAME} -p${DB_PASSWORD} ${db_name} < band.sql
+mysql -h ${DB_HOST} -D ${DB_SCHEMA} -P ${DB_PORT} -u ${DB_USERNAME} -p${DB_PASSWORD} < example_data.sql
+mysql -h ${DB_HOST} -D ${DB_SCHEMA} -P ${DB_PORT} -u ${DB_USERNAME} -p${DB_PASSWORD} < band.sql
 
 
 echo "end"
