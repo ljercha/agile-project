2d7f783fdbd1b5cd98434dd5969d9b6bffbde6d3
Oleksandr Gneushev <oleksandr.gneushev@kainos.com>
2023-08-11 14:34:26 +0200

    add local db creation config

--- product-backend/scripts/db_build.sh
+++ product-backend/scripts/db_build.sh
@@ -1,14 +1,9 @@
 #!/bin/zsh
-#source ~/.zshrc
 
 db_name="$DB_NAME"
 DB_PORT=3306
 
 mysql -h ${DB_HOST} -D ${DB_SCHEMA} -P ${DB_PORT} -u ${DB_USERNAME} -p${DB_PASSWORD} < create_db.sql
 mysql -h ${DB_HOST} -D ${DB_SCHEMA} -P ${DB_PORT} -u ${DB_USERNAME} -p${DB_PASSWORD} < band.sql
-
-# Uncomment if you need example data
-#  mysql -h ${DB_HOST} -D ${DB_SCHEMA} -P ${DB_PORT} -u ${DB_USERNAME} -p${DB_PASSWORD} < example_data.sql
-
 
 echo "end"
