c996de7c33c5eb20b92d8869bc05ea5915caa829
Lukasz Jercha <lukaszj@kainos.com>
2023-09-14 13:51:21 +0200

    refactor: removed unused methods

--- product-web-app/controller/productController.ts
+++ product-web-app/controller/productController.ts
@@ -47,49 +47,5 @@ export default class ProductController {
         res.render('add-product', req.body);
       }
     });
-
-    app.get('/add-product-name', async (req: Request, res: Response) => {
-      if (!req.session.product) {
-        req.session.product = {};
-      }
-      res.render('add-product-name');
-    });
-    app.post('/add-product-name', async (req: Request, res: Response) => {
-      req.session.product!.name = req.body.name;
-      res.redirect('/add-product-description');
-    });
-
-    app.get('/add-product-description', async (req: Request, res: Response) => {
-      res.render('add-product-description');
-    });
-    app.post('/add-product-description', async (req: Request, res: Response) => {
-      req.session.product!.description = req.body.description;
-      res.redirect('/add-product-price');
-    });
-
-    app.get('/add-product-price', async (req: Request, res: Response) => {
-      res.render('add-product-price');
-    });
-    app.post('/add-product-price', async (req: Request, res: Response) => {
-      req.session.product!.price = req.body.price;
-      res.redirect('/add-product-confirmation');
-    });
-
-    app.get('/add-product-confirmation', async (req: Request, res: Response) => {
-      res.render('add-product-confirmation', req.session.product);
-    });
-    app.post('/add-product-confirmation', async (req: Request, res: Response) => {
-      const data: Partial<Product> | undefined = req.session.product;
-
-      try {
-        const newProduct = await this.productService.createProduct(data as Product);
-        req.session.product = undefined;
-        res.redirect(`/products/${newProduct.productId}`);
-      } catch (e: any) {
-        console.error(e);
-        res.locals.errormessage = e.message;
-        res.render('add-product-confirmation', req.session.product);
-      }
-    });
   }
 }
